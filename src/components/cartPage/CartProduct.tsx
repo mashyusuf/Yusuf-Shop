7de8651164dd562ace61cdfc12f3370c3a648d74
MD Yusuf <yk7963838@gmail.com>
2024-11-23 16:42:57 +0600

    Your commit message

--- src/components/cartPage/CartProduct.tsx
+++ src/components/cartPage/CartProduct.tsx
@@ -2,8 +2,20 @@ import React from 'react'
 import { ProductType } from '../../../type'
 import Link from 'next/link'
 import Image from 'next/image'
+import { IoClose } from 'react-icons/io5';
+import AddToCartButton from '../ProductsCard/AddToCartButton';
+import PriceFormate from '../ProductsCard/PriceFormate';
+import { useDispatch } from 'react-redux';
+import { removeFromCart } from '@/redux/Redux Reducer/ReduxReducer';
+import toast from 'react-hot-toast';
+import { FaCheck } from 'react-icons/fa';
 
 function CartProduct({product}:{product:ProductType}) {
+  const dispatch = useDispatch();
+  const handleRemoveProduct = () => {
+    dispatch(removeFromCart(product?.id));
+    toast.success(`${product?.title.substring(0, 20)} deleted successfully!`);
+  };
   return (
     <div className='flex py-6 sm:py-10'>
         <Link
@@ -21,6 +33,58 @@ function CartProduct({product}:{product:ProductType}) {
           className="h-full w-full p-2 rounded-md object-contain bg-[#f7f7f7] hover:scale-110 duration-200"
         />
       </Link>
+      <div className="ml-4 sm:ml-6 flex flex-1 flex-col justify-between">
+        <div className="relative pr-9 sm:grid sm:grid-cols-4 sm:gap-x-6 sm:pr-0">
+          <div className="flex flex-col gap-1 col-span-3">
+            <h3 className="text-base font-semibold w-full">
+              {product?.title.substring(0, 80)}
+            </h3>
+            <p className="text-xs">
+              Brand: <span className="font-medium">{product?.brand}</span>
+            </p>
+            <p className="text-xs">
+              Category: <span className="font-medium">{product?.category}</span>
+            </p>
+            <div className="flex items-center gap-6 mt-2">
+              <PriceFormate
+                amount={product?.price * product?.quantity!}
+                className="text-base font-semibold"
+              />
+              <AddToCartButton product={product} className="gap-5" />
+            </div>
+          </div>
+          <div className="mt-4 sm:mt-0 sm:pr-9">
+            <div className="absolute right-0 top-0">
+              <button
+                onClick={handleRemoveProduct}
+                className=" p-2 text-gray-600 hover:text-red-600"
+              >
+                <IoClose />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div>
+        {product?.availabilityStatus && (
+            <p className="mt-4 flex space-x-2 text-sm text-gray-700">
+              <FaCheck className="text-lg text-green-500" />{" "}
+              <span>In Stock</span>
+            </p>
+          )}
+          <p className="text-sm">
+            You are saving{" "}
+            <PriceFormate
+              className="text-semibold text-green-500"
+              amount={
+                product?.price *
+                (product?.discountPercentage / 100) *
+                product.quantity!
+              }
+            />{" "}
+            upon purchase
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
